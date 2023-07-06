8a891da8ea489eb3e5c17226ade16876c74023cd
Neda <nchangizi@bcit.ca>
2023-07-06 16:24:33 -0700

    Added textinput and onChangeText prop in App.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./components/Header";
+import { useState } from "react";
 
 export default function App() {
   const appName = "CS 5220";
+  function handleChangeText(changedText) {
+    console.log(changedText);
+  }
   return (
     <View style={styles.container}>
       <Header name={appName} />
+      <TextInput style={styles.input} onChangeText={handleChangeText} />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,5 +23,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    borderBottomWidth: 1,
+    width: 200,
   },
 });
