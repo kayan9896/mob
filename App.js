cb935ee51d8dc7db5035af3326e96b5bdece3203
Neda <nchangizi@bcit.ca>
2023-07-20 15:52:11 -0700

    added GoalDetails and navigate to it on press

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "./components/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Input from "./components/Input";
+import GoalDetails from "./components/GoalDetails";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -10,6 +10,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Homepage" component={Home} />
+        <Stack.Screen name="Goal Details" component={GoalDetails} />
       </Stack.Navigator>
     </NavigationContainer>
   );
