d82d253123e999d19eea505e4da9223384419689
Neda <nchangizi@bcit.ca>
2023-07-20 15:32:08 -0700

    Home Screen added as first navigation screen

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Home from "./components/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Input from "./components/Input";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Homepage" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
