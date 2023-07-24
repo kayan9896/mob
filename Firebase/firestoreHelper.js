b112bc2008478ce4d6b133b53bd887044176ff71
Neda <nchangizi@bcit.ca>
2023-07-24 16:59:51 -0700

    delete from firestore added

--- Firebase/firestoreHelper.js
+++ Firebase/firestoreHelper.js
@@ -1,6 +1,13 @@
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
 import { database } from "./firebase-setup";
 
+export async function deleteFromDB(docID) {
+  try {
+    await deleteDoc(doc(database, "goals", docID));
+  } catch (err) {
+    console.log("delete doc ", err);
+  }
+}
 export async function writeToDB(goal) {
   try {
     const docRef = await addDoc(collection(database, "goals"), goal);
