f881c24fc1446663708bf3a265601c9943a9a781
Neda <nchangizi@bcit.ca>
2023-07-27 16:27:34 -0700

    post data

--- components/GoalUsers.js
+++ components/GoalUsers.js
@@ -1,4 +1,4 @@
-import { View, FlatList, Text } from "react-native";
+import { View, FlatList, Text, Button } from "react-native";
 import React, { useEffect, useState } from "react";
 
 export default function GoalUsers() {
@@ -26,8 +26,37 @@ export default function GoalUsers() {
     fetchUsers();
   }, []);
 
+  async function postNewUser() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+        {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify({ name: "Neda" }),
+        }
+      );
+      if (!response.ok) {
+        // throwing an error exist this function
+        throw new Error("Data was not written!");
+      }
+      const data = await response.json();
+      setUsers((prevUsers) => {
+        return [...prevUsers, data.name];
+      });
+      // const usersArray = data.map((item) => {
+      //   return item.name;
+      // });
+      // setUsers(usersArray);
+    } catch (err) {
+      // any of the promises above is rejected
+      console.log("post new user  ", err);
+    }
+  }
+
   return (
     <View>
+      <Button title="Add a new user" onPress={postNewUser} />
       <FlatList
         data={users}
         renderItem={({ item }) => {
