8153983e42b702053809ce4a148852074fe08a12
Neda <nchangizi@bcit.ca>
2023-07-31 16:37:34 -0700

    set userLoggedIn stat

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./components/Home";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import GoalDetails from "./components/GoalDetails";
 const Stack = createNativeStackNavigator();
-import { AntDesign } from "@expo/vector-icons";
-import PressableButton from "./components/PressableButton";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./Firebase/firebase-setup";
 
 export default function App() {
   const [isUserLoggedin, setIsUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      //the callback will be called on any authentication changes
+      if (user) {
+        setIsUserLoggedIn(true);
+      } else {
+        setIsUserLoggedIn(false);
+      }
+    });
+  }, []);
   //screens related for non authenticated users
   const AuthStack = (
     <>
